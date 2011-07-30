9db03059b17c0fb6f026d49ac5a8372d6fcd64ed
Jian Li <jianli130@gmail.com>
2011-07-29 23:02:03 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Travelphoto::Application.config.secret_token = 'a3efc69c7034d01577a535a79476602d1ef80cee2902046c00e51d2c4decaed71f74ef25340f0a9e683f15e97fad11c18fb21a69c966b02190f9ee34b8911b1f'
